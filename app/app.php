fa3087f556882b135aa52480e7c27d5023298226
mkelley2 <[m_kelley2@yahoo.com]>
2017-03-16 10:57:21 -0700

    add views to web

--- app/app.php
+++ app/app.php
@@ -24,7 +24,7 @@
 
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
-        'twig.path' => __DIR__.'/../views'
+        'twig.path' => __DIR__.'/../web/views'
     ));
 
     $app['debug'] = true;
